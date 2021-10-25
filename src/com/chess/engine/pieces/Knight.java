634650901952cbdf2629b39a0ebba7dcb9c0e413
a_Chav13 <anuj.chavan3101@gmail.com>
2021-10-25 14:24:23 -0700

    Knight Completed, Moves Pending, BoardUtils Created

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -2,8 +2,13 @@ package com.chess.engine.pieces;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
 import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Knight extends Piece{
@@ -15,15 +20,54 @@ public class Knight extends Piece{
     }
 
     @Override
-    public List<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(Board board) {
+        final List<Move> legalMoves = new ArrayList<>();
 
-        int candidateDestinationCoordinate;
+        for(final int currentCandidateOffset: CANDIDATE_MOVE_COORDINATES){
+            final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
 
-        for(final int currentCandidate: CANDIDATE_MOVE_COORDINATES){
-            candidateDestinationCoordinate = this.piecePosition + currentCandidate;
+            if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
 
+                if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isSecondColumnExclusion(this.piecePosition, currentCandidateOffset) ||
+                        isSeventhColumnExclusion(this.piecePosition, currentCandidateOffset)||
+                        isEightColumnExclusion(this.piecePosition, currentCandidateOffset)) {
+                    continue;
+                }
+
+                final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+
+                if(!candidateDestinationTile.isTileOccupied()){
+                    legalMoves.add(new Move());
+                }
+                else {
+                    final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                    final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+
+                    if(this.pieceAlliance!=pieceAlliance){
+                        legalMoves.add(new Move());// Editing move necessary
+                    }
+                }
+            }
         }
+        return ImmutableList.copyOf(legalMoves);
+    }
 
-        return null;
+    private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset==-17 || candidateOffset==-10 ||
+                candidateOffset==6 || candidateOffset==15);
+    }
+
+    private static boolean isSecondColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.SECOND_COLUMN[currentPosition] && (candidateOffset==-10 || candidateOffset==6);
+    }
+
+    private static boolean isSeventhColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.SEVENTH_COLUMN[currentPosition] && (candidateOffset==-6 || candidateOffset==10);
+    }
+
+    private static boolean isEightColumnExclusion(final int currentPosition, final int candidateOffset){
+        return BoardUtils.EIGHT_COLUMN[currentPosition] && (candidateOffset==-15 || candidateOffset==-6 ||
+                candidateOffset==10 || candidateOffset==17);
     }
 }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -4,7 +4,7 @@ import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 
-import java.util.List;
+import java.util.Collection;
 
 
 public abstract class Piece {
@@ -18,6 +18,10 @@ public abstract class Piece {
         this.pieceAlliance = pieceAlliance;
     }
 
-    public abstract List<Move> calculateLegalMoves(final Board board);
+    public Alliance getPieceAlliance(){
+        return this.pieceAlliance;
+    }
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
 
 }
