6228d0273183a1e537b10a5faafdfeb922ff2c94
a_Chav13 <anuj.chavan3101@gmail.com>
2021-11-01 03:23:10 -0700

    Queen Updated

--- src/com/chess/engine/pieces/Queen.java
+++ src/com/chess/engine/pieces/Queen.java
@@ -1,4 +1,5 @@
 package com.chess.engine.pieces;
+
 import  com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.BoardUtils;
@@ -10,11 +11,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static com.chess.engine.board.Move.*;
+
 public class Queen extends Piece {
+
     private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9,-8,-7,-1,1,7,8,9};
+
     public Queen(int piecePosition, Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
+
     @Override
     public Collection<Move> calculateLegalMoves(final Board board) {
 
@@ -34,14 +40,14 @@ public class Queen extends Piece {
                     final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
 
                     if(!candidateDestinationTile.isTileOccupied()){
-                        legalMoves.add(new Move.MajorMove(board,this,candidateDestinationCoordinate));
+                        legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
                     }
                     else {
                         final Piece pieceAtDestination = candidateDestinationTile.getPiece();
                         final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
 
                         if(this.pieceAlliance!=pieceAlliance){
-                            legalMoves.add(new Move.AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
+                            legalMoves.add(new AttackMove(board,this,candidateDestinationCoordinate,pieceAtDestination));
                         }
                         break;
                     }
